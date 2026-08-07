13b2d118e1132e0693ea07de1b7aa2d852af15ad
Sergio Adrian Flores Gutierrez <sergioflogu3@gmail.com>
2026-08-07 08:23:57 -0400

    Mapeo JPA: @table, class y referencia por id

--- proyecto/src/main/java/com/atlas/bank/atlas_bank/account/model/Account.java
+++ proyecto/src/main/java/com/atlas/bank/atlas_bank/account/model/Account.java
@@ -8,6 +8,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "accounts")
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,12 +18,26 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
+
+    @Column(name = "account_number",  nullable = false,  unique = true)
     private String accountNumber;
+
+    @Column(name = "owner_name", nullable = false)
     private String ownerName;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false, length = 20)
     private String type;  // SAVING, CHECKING,
+
+    @Column(nullable = false)
     private BigDecimal balance;
+
+    @Column(nullable = false, length = 20)
     private String status; // ACTIVE, CLOSED, FROZEN
+
+    @Column(name = "created_at",  nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @PrePersist
--- proyecto/src/main/java/com/atlas/bank/atlas_bank/transaction/model/Transaction.java
+++ proyecto/src/main/java/com/atlas/bank/atlas_bank/transaction/model/Transaction.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "transactions")
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -14,12 +15,26 @@ public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 20)
     private String type; // DEPOSIT, WITHDRAWAL, TRANSFER
+
+    @Column(name = "source_account_id", nullable = false)
     private Long sourceAccountId;
+
+    @Column(name = "target_account_id", nullable = false)
     private Long targetAccountId;
+
+    @Column(nullable = false)
     private BigDecimal amount;
+
+    @Column(nullable = false)
     private BigDecimal fee;
+
+    @Column(nullable = false, length = 20)
     private String status; // PENDING, EXECUTED, REJECTED
+
+    @Column(name = "created_at", nullable = false,  updatable = false)
     private LocalDateTime createdAt;
 
     @PrePersist
