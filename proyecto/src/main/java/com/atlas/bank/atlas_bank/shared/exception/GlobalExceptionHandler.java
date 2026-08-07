fe1195e6b72e1d5b22cfc0519f0ac1f22e4fd0b4
Sergio Adrian Flores Gutierrez <sergioflogu3@gmail.com>
2026-08-07 05:15:05 -0400

    RestControllerAdvice centralizado

---
+++ proyecto/src/main/java/com/atlas/bank/atlas_bank/shared/exception/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.atlas.bank.atlas_bank.shared.exception;
+
+import com.atlas.bank.atlas_bank.account.exception.AccountNotFoundException;
+import com.atlas.bank.atlas_bank.transaction.exception.AccountNotActiveException;
+import com.atlas.bank.atlas_bank.transaction.exception.InsufficientFoundsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ProblemDetail handleAccountNotFound(AccountNotFoundException exception){
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(
+                HttpStatus.NOT_FOUND, exception.getMessage()
+        );
+        problem.setTitle("Cuenta no encontrada");
+        return problem;
+    }
+
+    @ExceptionHandler(InsufficientFoundsException.class)
+    public ProblemDetail handleInsufficientFounds(InsufficientFoundsException exception){
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(
+                HttpStatusCode.valueOf(422), exception.getMessage()
+        );
+        problem.setTitle("Fondos insuficientes para realizar esta operacion");
+        return problem;
+    }
+
+    @ExceptionHandler(AccountNotActiveException.class)
+    public ProblemDetail handleAccountNotActive(AccountNotActiveException exception){
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(
+                HttpStatusCode.valueOf(422), exception.getMessage()
+        );
+        problem.setTitle("Cuenta no activa");
+        return problem;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleException(Exception exception){
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(
+                HttpStatus.INTERNAL_SERVER_ERROR, "Error interno del servidor"
+        );
+        problem.setTitle("Error interno");
+        return problem;
+    }
+}
