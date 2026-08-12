5905f4ea4a070b5ef0bcb8bf41653f90f8528596
Sergio Adrian Flores Gutierrez <sergioflogu3@gmail.com>
2026-08-12 06:14:27 -0400

    SecurityFilterChain proteccion basada en rol 2

--- proyecto/src/main/java/com/atlas/bank/atlas_bank/shared/config/SecurityConfig.java
+++ proyecto/src/main/java/com/atlas/bank/atlas_bank/shared/config/SecurityConfig.java
@@ -7,7 +7,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -38,7 +43,23 @@ public class SecurityConfig {
                 .headers(headers ->
                         headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .csrf(AbstractHttpConfigurer::disable);
-        
+
         return http.build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var realmAccess = jwt.getClaimAsMap("realm_access");
+            if (realmAccess == null  || realmAccess.get("roles") == null) {
+                return List.of();
+            }
+            var roles = (List<String>) realmAccess.get("roles");
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority(role))
+                    .collect(Collectors.toList());
+
+        });
+        return converter;
     }
 }
